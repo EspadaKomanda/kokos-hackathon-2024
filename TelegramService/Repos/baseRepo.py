78d270d5d4919dd73a633090aaa5fe78a63fecaa
Nikolai Papin <nikolai@weirdcat.ru>
2024-10-07 20:24:43 +0300

    baseRepo + empty repo files

---
+++ TelegramService/Repos/baseRepo.py
@@ -0,0 +1,21 @@
+class BaseRepo:
+
+    model = None
+
+    def create(self, **kwargs):
+        """Create a new record."""
+        return self.model.create(**kwargs)
+
+    def getById(self, record_id):
+        """Read a record by ID."""
+        return self.model.get_by_id(record_id)
+
+    def update(self, record_id, **kwargs):
+        """Update a record by ID."""
+        query = self.model.update(**kwargs).where(self.model.id == record_id)
+        query.execute()
+
+    def delete(self, record_id):
+        """Delete a record by ID."""
+        query = self.model.delete().where(self.model.id == record_id)
+        query.execute()
