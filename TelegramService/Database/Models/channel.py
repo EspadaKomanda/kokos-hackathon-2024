71e24c2e4735a781fa2381f21a3d602296898106
Nikolai Papin <nikolai@weirdcat.ru>
2024-10-10 18:12:58 +0300

    Added channel name field in the channel model

--- TelegramService/Database/Models/channel.py
+++ TelegramService/Database/Models/channel.py
@@ -1,5 +1,5 @@
 from peewee import AutoField, BigIntegerField, BooleanField, IntegerField
-from peewee import Check
+from peewee import CharField, Check
 
 from Database.Models.baseModel import BaseModel
 
@@ -8,5 +8,6 @@ class Channel(BaseModel):
 
     id = AutoField(primary_key=True)
     chat_id = BigIntegerField(unique=True)
+    channel_name = CharField()
     enable_sniff = BooleanField(default=False)
     sniff_delay = IntegerField(default=60, constraints=[Check("sniff_delay > 9")])
