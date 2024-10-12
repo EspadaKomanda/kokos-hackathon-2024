70bc0023f4fc47be6c673c49ffd6a020adc6962d
ereshkigal <roma.hlinin@yandex.ru>
2024-10-12 17:19:34 +0300

    fixes in db

---
+++ Team.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchService.Database.Models
+{
+    public class Team
+    {
+        [Key]
+        public long TeamId {get;set;}
+        [Required]
+        public string Name {get;set;}
+        public ICollection<Player>? Players {get;set;}
+   }
+}
